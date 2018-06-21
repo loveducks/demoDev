90e1555fcc43341e07da7da7b655f748c8540f1f
nano <nano@debian.nano>
2018-06-21 09:14:03 -0400

    page visibility

--- js/logic.js
+++ js/logic.js
@@ -8,3 +8,30 @@ window.addEventListener('deviceorientation', (e) => {
         'rotateY(' +e.gamma+ 'deg)' +
         'rotateZ(' +e.alpha+ 'deg)';
 });
+
+const getPrefix = ()=> {
+    let prefix = null;
+    if(document.hidden !== undefined)
+        prefix = "";
+    else{
+        let browserPrefixes = [ 'webkit', 'moz', 'ms', 'o' ];
+        for( let i = 0; i < browserPrefixes.length; i++ )
+            if(document[browserPrefixes[i] + 'Hidden'] != undefined){
+                prefix = browserPrefixes[i];
+                break;
+            }
+    }
+
+    return prefix;
+}
+
+const countView = ()=>{
+    if( document.hidden === false || document[prefix + 'Hidden'] === false )
+        views++;
+    console.log(views);
+}
+
+let views = 0;
+let prefix = getPrefix();
+window.onload = countView();
+
