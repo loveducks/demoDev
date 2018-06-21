7785229089f62d58da8fc2cd0863ee08c0958ee4
nano <nano@debian.nano>
2018-06-21 05:00:01 -0400

    first test commit

---
+++ js/logic.js
@@ -0,0 +1,8 @@
+let blob = document.querySelector('.blog');
+
+window.addEventListener('deviceorientation', (e) => {
+    let tiltLR = e.gamma;
+    let tiltFB = e.beta;
+
+    blog.style.transform = `rotate(${tiltLR}deg) rotate3d(1, 0, 0, ${tiltFB * -1}deg)`; 
+});
