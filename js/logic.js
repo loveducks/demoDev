1883478df727ec371945724b2b190ef8f3e88f43
nano <nano@debian.nano>
2018-06-21 05:25:05 -0400

    fixes

--- js/logic.js
+++ js/logic.js
@@ -4,7 +4,7 @@ window.addEventListener('deviceorientation', (e) => {
     let tiltLR = e.gamma;
     let tiltFB = e.beta;
 
-    blog.style.transform = 'rotateX(' +e.beta+ 'deg)' +
+    blob.style.transform = 'rotateX(' +e.beta+ 'deg)' +
         'rotateY(' +e.gamma+ 'deg)' +
         'rotateZ(' +e.alpha+ 'deg)';
 });
